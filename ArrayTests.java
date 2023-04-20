e97a2086c24e56d7deb444cffb75cd04b0834946
rachelhandran <rachel.handran@gmail.com>
2023-04-20 16:44:18 -0700

    add all

---
+++ ArrayTests.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class ArrayTests {
+	@Test 
+	public void testReverseInPlace() {
+    int[] input1 = { 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3 }, input1);
+	}
+
+  @Test
+  public void testReversed() {
+    int[] input1 = { };
+    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  //Failing tests
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = { 3, 2, 1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1, 2, 3}, input1);
+	}
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {3, 2, 1};
+    assertArrayEquals(new int[]{1, 2, 3}, ArrayExamples.reversed(input1));
+  }
+}
